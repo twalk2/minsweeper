fc81dd6db4c8a124cd33d387f0e1f62f1348fe62
twalk2 <2twalk2@gmail.com>
2019-11-08 14:01:47 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  let display = false;
+  const [bgColor, setBgColor] = React.useState("");
+  const [matrix, setMatrix] = React.useState("");
+  const [score, setScore] = React.useState(0);
+
+  const neutralClick = event => {
+    event.target.disabled = true;
+    setScore(score + 1);
+  };
+
+  const alertDeath = () => {
+    alert("You lost LMAO");
+  };
+
+  const betterArray = () => {
+    let bestBetterArray = [];
+    let exes = 0;
+    for (let i = 0; i < 10; i++) {
+      let betterArray = [];
+      for (let j = 0; j < 10; j++) {
+        let maybeMine = Math.ceil(Math.random() * 7);
+        if (exes < 35) {
+          if (maybeMine == 7) {
+            exes += 1;
+            betterArray.push(
+              <button
+                key={"x"}
+                onClick={() => {
+                  alertDeath();
+                }}
+                className="bomb"
+                name={[i, j]}
+              ></button>
+            );
+          } else {
+            betterArray.push(
+              <button
+                className="neutral"
+                disabled={false}
+                onClick={neutralClick}
+                name={[]}
+              >
+                {display ? maybeMine : null}
+              </button>
+            );
+          }
+        } else {
+          betterArray.push(<button></button>);
+        }
+      }
+      bestBetterArray.push(betterArray);
+      console.log("The array", bestBetterArray);
+    }
+    return bestBetterArray;
+  };
+  const renderBetterArray = () => {
+    let theThing = betterArray();
+    return theThing.map(item => {
+      return <div className="row">{item}</div>;
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Minesweeper</h1>
+      <h2>Score: {score}</h2>
+      <div className="mine-wrapper">
+        {/* <div>{buttonRender()}</div>  */}
+
+        <div>{renderBetterArray()}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
